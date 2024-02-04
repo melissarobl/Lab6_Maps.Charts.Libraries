2e27b7181335c7621b69050207419589780f1bd3
m <example@example.com>
2024-02-03 21:41:40 -0600

    Bridges with two colors

--- longest_bridges_map_lab6/bridge_maps.js
+++ longest_bridges_map_lab6/bridge_maps.js
@@ -3,7 +3,7 @@ let usCenterCoordinates = [39.83, -98.35]
 let zoomLevel = 4 //1 = whole world to 20 = city blocks
 
 let map = L.map('bridge-map').setView(usCenterCoordinates, zoomLevel)
-//variable L created in Leaflet file in HTML  //'college-map' tells you where map is drawn (inside div with that name)
+//variable L created in Leaflet file in HTML  //'bridge-map' tells you where map is drawn (inside div with that name)
 
 //mapbox, maps.stamen.com, openStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -21,31 +21,46 @@ long_bridges =  [
 ]
 
 //loop over array to create the markers and popups
-long_bridges.forEach(function(long_bridge){
+long_bridges.forEach(function(long_bridge) {
     let markerText = `<b>${long_bridge.name}</b><br>Span: ${long_bridge.span}m` //Each marker should have a popup with the bridge's name and span length.
 
-    let bridgeBasicIcon = L.icon({
-        iconUrl: 'bridge.png',
-        iconSize:     [40, 75], // size of the icon
-        iconAnchor:   [9, 50], // point of the icon which will correspond to marker's location
-        popupAnchor:  [-2, -60] // point from which the popup should open relative to the iconAnchor
+    //format from https://leafletjs.com/examples/custom-icons/ for setting up for more than one icon:
+    let bridgeIcon = L.Icon.extend({
+        options: {
+            iconSize: [40, 75], // size of the icon
+            iconAnchor: [9, 50], // point of the icon which will correspond to marker's location
+            popupAnchor: [-2, -60] // point from which the popup should open relative to the iconAnchor
+        }
     })
 
+    let bridgeSpansArray = []  //create an empty array for holding bridge spans
+    long_bridges.forEach(function (bridge) { //loop through long_bridges list. Each bridge is one loop
+        bridgeSpansArray.push(bridge.span)  //add each bridge span to the array
+    })
+    //find the largest span:
+    let largestSpan = Math.max(...bridgeSpansArray)  //from stackoverflow: https://stackoverflow.com/questions/54623431/find-the-biggest-number-in-an-array-by-using-javascript-loops
+    let largestSpanBridgeName = long_bridges.find(bridge => bridge.span === largestSpan)  //format found in map_filter.js example from videos for this lesson
 
+    //set variables for each icon (format given from leafletjs.com):
+    let bridgeBasicIcon = new bridgeIcon({iconUrl: 'bridge.png'})
+    let longestBridgeIcon = new bridgeIcon({iconUrl: 'bridge_purple.png'})
+
+    //use if statement to determine which icon to use:
+    if (long_bridge === largestSpanBridgeName) {
+        L.marker(long_bridge.coordinates, {icon: longestBridgeIcon}).addTo(map).bindPopup(markerText)
+    } else {
         L.marker(long_bridge.coordinates, {icon: bridgeBasicIcon}).addTo(map).bindPopup(markerText)
+    }
+
         //L.marker(long_bridge.coordinates).bindPopup(markerText).addTo(map)  //just regular markers
 })
 
-
-
-    // let longestBridgeIcon = L.icon.extend({
-    //     options: {
-    // iconSize:     [40, 75], // size of the icon
-    // iconAnchor:   [9, 50], // point of the icon which will correspond to marker's location
-    // popupAnchor:  [-2, -60] // point from which the popup should open relative to the iconAnchor
-    //     }
-    // })
-
-
+//for just one icon:
+// let bridgeBasicIcon = L.icon({
+//     iconUrl: 'bridge.png',
+//     iconSize:     [40, 70], // size of the icon
+//     iconAnchor:   [9, 50], // point of the icon which will correspond to marker's location
+//     popupAnchor:  [-2, -60] // point from which the popup should open relative to the iconAnchor
+// })
 
 
